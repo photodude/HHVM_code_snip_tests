40843f80c84dc29060c16ccaf4ed923742c5ed11
Walt Sorensen <photodude@users.noreply.github.com>
2016-09-10 14:33:32 -0600

    $prePop = 50000000;
    100,000,000 was too big failed before starting
    
    only print every 100 elements

--- testMemory.php
+++ testMemory.php
@@ -1,6 +1,6 @@
 <?php
 $var1 = array();
-$prePop = 100000000;
+$prePop = 50000000;
 
 
 for ($i = 0; $i < $prePop; $i++)
@@ -8,16 +8,28 @@ for ($i = 0; $i < $prePop; $i++)
 	$var1[] = "this is a short string";
 }
 
+$r = 0;
+
 for ($i = 0; $i < PHP_INT_MAX - 10000; $i++)
 {
 	$var1[] = "this is a short string";
 	$e = $prePop + $i;
-	echo "Memory usage for $e elements = " . memory_get_usage() / 1048576 . " MB\n";
+	if ($i = $r + 100)
+	{
+		echo "Memory usage for $e elements = " . memory_get_usage() / 1048576 . " MB\n";
+		$r = $i;
+	}
 }
+
+$r = sizeof($var1);
 
 while ($i = sizeof($var1))
 {
-	echo "Memory usage for $i elements = " . memory_get_usage() / 1048576 . " MB\n";
+	if ($i = $r - 100 || $r < 100)
+	{
+		echo "Memory usage for $e elements = " . memory_get_usage() / 1048576 . " MB\n";
+		$r = $i;
+	}
 	unset($var1[$i -1]);
 }
 	echo "Maximum Memory usage = " . memory_get_peak_usage(true) / 1048576 . " MB\n";
