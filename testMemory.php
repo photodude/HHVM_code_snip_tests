4feacad5448b64f047d1f44858951a662239ad32
Walt Sorensen <photodude@users.noreply.github.com>
2016-09-10 17:17:25 -0600

    try print outs at every 10,000

--- testMemory.php
+++ testMemory.php
@@ -1,6 +1,6 @@
 <?php
 $var1 = array();
-$prePop = 368036506;
+$prePop = 268036506;
 
 
 for ($i = 0; $i < $prePop; $i++)
@@ -15,7 +15,7 @@ for ($i = 0; $i < PHP_INT_MAX - 10000; $i++)
 	$var1[] = "this is a short string";
 	$e = $prePop + $i;
 
-	if ($i = $r + 1000)
+	if ($i = $r + 10000)
 	{
 		//echo "Memory usage for $e elements = " . memory_get_usage() / 1048576 . " MB\n";
 		$r = $i;
