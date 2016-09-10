55182a136782a806d59434d3c6afddd87c9c559d
Walt Sorensen <photodude@users.noreply.github.com>
2016-09-10 13:51:33 -0600

    Create testMemory.php
    Script based on
    https://books.google.com/books?id=G_djDAAAQBAJ&pg=PA212&dq=memory_get_peak_usage()&hl=en&sa=X&ved=0ahUKEwj26dWb6PvOAhVI2mMKHYcTCWwQ6AEIKzAC#v=onepage&q=memory_get_peak_usage()&f=false

---
+++ testMemory.php
@@ -0,0 +1,15 @@
+<? php
+$var1 = array();
+
+for ($i = 0; $i < PHP_INT_MAX - 10000; $i++)
+{
+	$var1[] = "this is a short string";
+	echo "Memory usage for $i elements = " . memory_get_usage() / 1048576 . " MB\n";
+}
+
+while ($i = sizeof($var1))
+{
+	echo "Memory usage for $i elements = " . memory_get_usage() / 1048576 . " MB\n";
+	unset($var1[$i -1]
+}
+	echo "Maximum Memory usage = " . memory_get_peak_usage(true) / 1048576 . " MB\n";
