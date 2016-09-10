99ab17789e9e2f5a313ac3896def33750dff3378
Walt Sorensen <photodude@users.noreply.github.com>
2016-09-10 14:06:31 -0600

    missing ;

--- testMemory.php
+++ testMemory.php
@@ -10,6 +10,6 @@ for ($i = 0; $i < PHP_INT_MAX - 10000; $i++)
 while ($i = sizeof($var1))
 {
 	echo "Memory usage for $i elements = " . memory_get_usage() / 1048576 . " MB\n";
-	unset($var1[$i -1]
+	unset($var1[$i -1];
 }
 	echo "Maximum Memory usage = " . memory_get_peak_usage(true) / 1048576 . " MB\n";
