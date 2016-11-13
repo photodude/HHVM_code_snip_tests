2cce18343e8eb9b061c0ffa3a67fd0a3fa0f8fbe
Walt Sorensen <photodude@users.noreply.github.com>
2016-11-12 21:35:57 -0700

    drop _Dir_

--- testBadParse.php
+++ testBadParse.php
@@ -1,5 +1,5 @@
 <?php
-$filename = __DIR__ . 'bad.ini'; // The bad.ini file
+$filename = 'bad.ini'; // The bad.ini file
 $contents = file_get_contents($filename);
 $contents = str_replace('_QQ_', '"\""', $contents);
 $strings = @parse_ini_string($contents);
