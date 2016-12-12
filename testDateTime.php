29d859509782ffb7fd9e02dbf2b2ad7a866181ad
Walt Sorensen <photodude@users.noreply.github.com>
2016-12-11 20:25:06 -0700

    TimeZone causes the error

--- testDateTime.php
+++ testDateTime.php
@@ -1,11 +1,11 @@
 <?php
 
-$dateTime = new DateTime('2016-10-29 10:00:00');
+$dateTime = new DateTime('2016-10-29 10:00:00', new DateTimeZone('Europe/Amsterdam'));
 var_dump('created :' . $dateTime->format('Y-m-d\TH:i:s.u')); // 2016-10-29 10:00:00
 $dateTime->add(new DateInterval('P1D'));
 var_dump($dateTime); // 2016-10-30 10:00
 
-$dateTime = new DateTime('2016-10-29 10:00:00');
+$dateTime = new DateTime('2016-10-29 10:00:00', new DateTimeZone('Europe/Amsterdam'));
 var_dump('created :' . $dateTime->format('Y-m-d\TH:i:s.u')); // 2016-10-29 10:00:00
 $dateTime->add(new DateInterval('PT24H'));
 var_dump($dateTime); // 2016-10-30 09:00
