1cbe5a4f9a25e4cd76b945ad1976e35c54154703
Walt Sorensen <photodude@users.noreply.github.com>
2016-12-11 20:11:28 -0700

    need format to var dump a created object

--- testDateTime.php
+++ testDateTime.php
@@ -1,11 +1,11 @@
 <?php
 
 $dateTime = new DateTime('2016-10-29 10:00:00');
-var_dump('created :' . $dateTime); // 2016-10-29 10:00:00
+var_dump('created :' . $dateTime->format('u')); // 2016-10-29 10:00:00
 $dateTime->add(new DateInterval('P1D'));
 var_dump($dateTime); // 2016-10-30 10:00
 
 $dateTime = new DateTime('2016-10-29 10:00:00');
-var_dump('created :' . $dateTime); // 2016-10-29 10:00:00
+var_dump('created :' . $dateTime->format('u')); // 2016-10-29 10:00:00
 $dateTime->add(new DateInterval('PT24H'));
 var_dump($dateTime); // 2016-10-30 09:00
