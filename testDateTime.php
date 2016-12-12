8fd2ddf4333bf39164e44005bd34a3c82d170f1a
Walt Sorensen <photodude@users.noreply.github.com>
2016-12-11 19:45:36 -0700

    Create testDateTime.php

---
+++ testDateTime.php
@@ -0,0 +1,9 @@
+<?php
+
+$dateTime = new DateTime('2016-10-29 10:00:00');
+$dateTime->add(new DateInterval('P1D'));
+var_dump($dateTime); // 2016-10-30 10:00
+
+$dateTime = new DateTime('2016-10-29 10:00:00');
+$dateTime->add(new DateInterval('PT24H'));
+var_dump($dateTime); // 2016-10-30 09:00
